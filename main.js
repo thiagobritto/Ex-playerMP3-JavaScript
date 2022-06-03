43b8db1f13f99feb26a34be0332e558f1f6da321
thiagobritto <thiagobritto.developer@gmail.com>
2022-06-03 16:45:30 -0300

    set playlist

--- main.js
+++ main.js
@@ -1,23 +1,36 @@
 import player from "./modules/play-audio/player.js";
+import playlist from "./playlist.js"
 
-player.playlist([
-  'https://65381g.ha.azioncdn.net/5/b/6/6/joaogomescantor-meu-cafofo-746c2fe7.mp3',
-  'https://65381g.ha.azioncdn.net/8/a/8/f/osbaroesdapisadinha-baroes-da-pisadinha-galera-do-interior-dfbc6061.mp3'
-])
+const list = playlist.map(res => res.audio)
+
+function setup(index) {
+  capa.src = playlist[index].image
+  artista.innerHTML = playlist[index].artist
+  musica.innerHTML = playlist[index].music
+}
+
+setup(0)
+
+/**
+ * player mp3
+ */
+
+player.playlist(list)
 
 const playButton = document.getElementById('btnPlay')
 player.playButton(playButton, res => {
   playButton.innerHTML = res.pause ? 'play_circle_outline' : 'pause_circle_outline'
+  setup(res.track)
 })
 
 const nextButton = document.getElementById('btnNext')
-player.nextButton(nextButton, item => {
-  console.log(item);
+player.nextButton(nextButton, res => {
+  setup(res.track)
 })
 
 const prevButton = document.getElementById('btnPrev')
-player.prevButton(prevButton, item => {
-  console.log(item);
+player.prevButton(prevButton, res => {
+  setup(res.track)
 })
 
 const displayDurationTime = document.getElementById('displayDuration')
---
+++ playlist.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    artist: 'Artista 1',
+    music: 'primeira musica',
+    audio: 'https://65381g.ha.azioncdn.net/5/b/6/6/joaogomescantor-meu-cafofo-746c2fe7.mp3',
+    image: 'https://via.placeholder.com/300x300.png?text=imagem+01'
+  },
+  {
+    artist: 'Artista 2',
+    music: 'segunda musica',
+    audio: 'https://65381g.ha.azioncdn.net/8/a/8/f/osbaroesdapisadinha-baroes-da-pisadinha-galera-do-interior-dfbc6061.mp3',
+    image: 'https://via.placeholder.com/300x300.png?text=imagem+02'
+  }
+]
